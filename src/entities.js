a7484695138c939c9ef13b602ba83f8a3468c7db
fran <ordonez.f93@gmail.com>
2017-10-08 20:46:33 -0300

    converted identations into spaces

--- src/entities.js
+++ src/entities.js
@@ -2,70 +2,70 @@ var parser = require('parser');
 
 
 var Fact = function (parsedFact) {
-	
-	this.name = parsedFact[0];
-	this.args = parsedFact[1];
 
-	this.isEqual = function(query) {
-		return (this.name === query.getName() && this.args.join(',') === query.getArgs().join(','));
-	}
+    this.name = parsedFact[0];
+    this.args = parsedFact[1];
+
+    this.isEqual = function(query) {
+        return (this.name === query.getName() && this.args.join(',') === query.getArgs().join(','));
+    }
 }
 
 var Rule = function () {}
 
 var Query = function(parsedQuery) {
 
-	this.name = parsedQuery[0];
-	this.args = parsedQuery[1];
+    this.name = parsedQuery[0];
+    this.args = parsedQuery[1];
 
-	this.getName = function() {return this.name;}
-	this.getArgs = function() {return this.args;}
+    this.getName = function() {return this.name;}
+    this.getArgs = function() {return this.args;}
 }
 
 var DataBase = function() {
 
-	this.facts = [];
-	this.rules = [];
+    this.facts = [];
+    this.rules = [];
 
-	this.factParser = new parser.FactParser();
-	this.ruleParser = new parser.RuleParser();
-	this.queryParser = new parser.QueryParser();
+    this.factParser = new parser.FactParser();
+    this.ruleParser = new parser.RuleParser();
+    this.queryParser = new parser.QueryParser();
 
-	this.createDataBase = function(db) {
-		for (let row of db) {
-			if (this.factParser.isFact(row)) {
-				let factComponents = this.factParser.parse(row);
-				this.facts.push(new Fact(factComponents));
+    this.createDataBase = function(db) {
+        for (let row of db) {
+            if (this.factParser.isFact(row)) {
+                let factComponents = this.factParser.parse(row);
+                this.facts.push(new Fact(factComponents));
 
-			} else if (this.ruleParser.isRule(row)) {
-				let ruleComponents = this.ruleParser.parse(row);
-				this.rules.push(new Rule(ruleComponents));
+            } else if (this.ruleParser.isRule(row)) {
+                let ruleComponents = this.ruleParser.parse(row);
+                this.rules.push(new Rule(ruleComponents));
 
-			} else {return null;}
-		}
-	}
+            } else {return null;}
+        }
+    }
 
-	this.checkForFact = function(query) {
+    this.checkForFact = function(query) {
 
-		let res = this.facts.map(fact => fact.isEqual(query));
-		return res.includes(true);
-	}
+        let res = this.facts.map(fact => fact.isEqual(query));
+        return res.includes(true);
+    }
 
-	this.checkForRule = function(query) {/*Do Stuff*/}
+    this.checkForRule = function(query) {/*Do Stuff*/}
 
-	this.checkQuery = function(query) {
+    this.checkQuery = function(query) {
 
-		if (this.queryParser.isQuery(query)) {
-			let queryComponents = this.queryParser.parse(query);
-			let queryObject = new Query(queryComponents);
+        if (this.queryParser.isQuery(query)) {
+            let queryComponents = this.queryParser.parse(query);
+            let queryObject = new Query(queryComponents);
 
-			return this.checkForFact(queryObject) || this.checkForRule(queryObject);
+            return this.checkForFact(queryObject) || this.checkForRule(queryObject);
 
-		} else {return null;}
-	}
+        } else {return null;}
+    }
 }
 
 
 module.exports = {
-	DataBase
+    DataBase
 }
--- src/interpreter.js
+++ src/interpreter.js
@@ -1,11 +1,18 @@
+var Entities = require('entities');
+var DataBase = Entities.DataBase;
+
+
 var Interpreter = function () {
+    this.database;
 
-    this.parseDB = function (params, paramss, paramsss) {
-
+    this.parseDB = function(db) {
+        this.database = new DataBase();
+        this.database.createDataBase(db);
+        if (this.database === null) {return null};
     }
 
-    this.checkQuery = function (params) {
-        return true;
+    this.checkQuery = function (query) {
+        return this.database.checkQuery(query);
     }
 
 }
