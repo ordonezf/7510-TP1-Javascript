06efd2e5b92090d59bbf7ba26ac28c567e15fed8
ordonezf <ordonez.f93@gmail.com>
2017-10-09 13:12:00 -0300

    parser tests

---
+++ test/parserTests.js
@@ -0,0 +1,138 @@
+var expect = require("chai").expect;
+var should = require('should');
+var assert = require('assert');
+
+var parser = require('../src/parser');
+
+var FactParser = parser.FactParser;
+var QueryParser = parser.QueryParser;
+var RuleParser = parser.RuleParser;
+
+describe("Parser", function () {
+
+    var db = [
+        "varon(juan).",
+        "varon(pepe).",
+        "varon(hector).",
+        "varon(roberto).",
+        "varon(alejandro).",
+        "mujer(maria).",
+        "mujer(cecilia).",
+        "padre(juan, pepe).",
+        "padre(juan, pepa).",
+        "padre(hector, maria).",
+        "padre(roberto, alejandro).",
+        "padre(roberto, cecilia).",
+        "hijo(X, Y) :- varon(X), padre(Y, X).",
+        "hija(X, Y) :- mujer(X), padre(Y, X)."
+    ];
+
+    var factParser = null;
+    var queryParser = null;
+    var ruleParser = null;
+
+    beforeEach(function () {
+        // runs before each test in this block
+        factParser = new FactParser();
+        queryParser = new QueryParser();
+        ruleParser = new RuleParser();
+    });
+
+    describe('FactParser', function () {
+
+        it('varon(juan). should be true', function () {
+            assert(factParser.isFact('varon(juan).') === true);
+        });
+
+        it('varon(juan. should be false', function () {
+            assert(factParser.isFact('varon(juan.') === false);
+        });
+
+        it('varon(maria) should be false', function () {
+            assert(factParser.isFact('varon(maria)') === false);
+        });
+
+        it('padre(juan, pepe). should be true', function () {
+            assert(factParser.isFact('padre(juan, pepe).') === true);
+        });
+
+        it('padre(mario, Pepe). should be false', function () {
+            assert(factParser.isFact('padre(mario, Pepe).') === false);
+        });
+
+        it('padre(mario,pepe). should be false', function () {
+            assert(factParser.isFact('padre(mario,Pepe).') === false);
+        });
+
+        it('padre mario, pepe). should be false', function () {
+            assert(factParser.isFact('padre mario, pepe).') === false);
+        });
+
+    });
+
+    describe('QueryParser', function () {
+
+        it('varon(juan) should be true', function () {
+            assert(queryParser.isQuery('varon(juan)') === true);
+        });
+
+        it('varon(juan should be false', function () {
+            assert(queryParser.isQuery('varon(juan.') === false);
+        });
+
+        it('varon(maria) should be true', function () {
+            assert(queryParser.isQuery('varon(maria)') === true);
+        });
+
+        it('padre(juan, pepe) should be true', function () {
+            assert(queryParser.isQuery('padre(juan, pepe)') === true);
+        });
+
+        it('padre(mario, Pepe) should be false', function () {
+            assert(queryParser.isQuery('padre(mario, Pepe)') === false);
+        });
+
+        it('padre(mario,pepe) should be false', function () {
+            assert(queryParser.isQuery('padre(mario,Pepe)') === false);
+        });
+
+        it('padre mario, pepe). should be false', function () {
+            assert(queryParser.isQuery('padre mario, pepe)') === false);
+        });
+
+    });
+
+    describe('RuleParser', function () {
+
+        it('hijo(X, Y) :- varon(X), padre(Y, X). should be true', function () {
+            assert(ruleParser.isRule('hijo(X, Y) :- varon(X), padre(Y, X).') === true);
+        });
+
+        it('hijo(X, Y) - varon(X), padre(Y, X). should be false', function () {
+            assert(ruleParser.isRule('hijo(X, Y) - varon(X), padre(Y, X).') === false);
+        });
+
+        it('hijo(X, Y) :- padre(Y, X). should be true', function () {
+            assert(ruleParser.isRule('hijo(X, Y) :- padre(Y, X).') === true);
+        });
+
+        it('hijo(X, Y, Z) :- varon(Z, X), padre(Y, X). should be true', function () {
+            assert(ruleParser.isRule('hijo(X, Y, Z) :- varon(Z, X), padre(Y, X).') === true);
+        });
+
+        it('hijo(X, Y, z) :- varon(Z, X), padre(Y, X). should be false', function () {
+            assert(ruleParser.isRule('hijo(X, Y, z) :- varon(Z, X), padre(Y, X).') === false);
+        });
+
+        it('hijo(X, Y, Z) :- varon(Z, X), padre(Y, X) should be false', function () {
+            assert(ruleParser.isRule('hijo(X, Y, Z) :- varon(Z, X), padre(Y, X)') === false);
+        });
+
+        it('hijo(X, Y, Z) : varon(Z, X), padre(Y, X). should be false', function () {
+            assert(ruleParser.isRule('hijo(X, Y, Z) : varon(Z, X), padre(Y, X).') === false);
+        });
+
+    });
+
+
+});
