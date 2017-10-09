dcfc9dbdb397795672d909e6983c9821331fb3fc
ordonezf <ordonez.f93@gmail.com>
2017-10-09 14:08:23 -0300

    removed unused db

--- test/parserTests.js
+++ test/parserTests.js
@@ -10,23 +10,6 @@ var RuleParser = parser.RuleParser;
 
 describe("Parser", function () {
 
-    var db = [
-        "varon(juan).",
-        "varon(pepe).",
-        "varon(hector).",
-        "varon(roberto).",
-        "varon(alejandro).",
-        "mujer(maria).",
-        "mujer(cecilia).",
-        "padre(juan, pepe).",
-        "padre(juan, pepa).",
-        "padre(hector, maria).",
-        "padre(roberto, alejandro).",
-        "padre(roberto, cecilia).",
-        "hijo(X, Y) :- varon(X), padre(Y, X).",
-        "hija(X, Y) :- mujer(X), padre(Y, X)."
-    ];
-
     var factParser = null;
     var queryParser = null;
     var ruleParser = null;
